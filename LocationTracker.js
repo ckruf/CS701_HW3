3ce39e66c96b13bce6638f3b5edd8a7da9e492eb
christian <christian.kruf@pwc.com>
2022-02-20 10:53:11 +0100

    Random location implemented

--- LocationTracker.js
+++ LocationTracker.js
@@ -3,80 +3,85 @@ window.onload = init;
 // register the event handler for button
 
 function init() {
-	var startButton = document.getElementById("startButton");
-	//startButton.onclick = getLocation;
-    //startButton.onclick = disableButton(startButton);
-    startButton.addEventListener("click",disableButton);
-    startButton.addEventListener("click",getLocation);
-    position = 
-    setInterval(function(){ updateMyLocation(); },5000);
-
+  var startButton = document.getElementById("startButton");
+  //startButton.onclick = getLocation;
+  //startButton.onclick = disableButton(startButton);
+  startButton.addEventListener("click", disableButton);
+  startButton.addEventListener("click", getLocation);
+  setInterval(function () {
+    updateMyLocation();
+  }, 5000);
 
   /*  $('#startButton').one('submit', function() {
         $(this).find('input[type="submit"]').attr('disabled','disabled');
     }); */
-    
 }
 
-function disableButton(){
-    startButton.disabled = "true";
+function disableButton() {
+  startButton.disabled = "true";
 }
 
-function updateMyLocation(position){
-    var currentLatitude = document.getElementById("currentLatitude");
-    var currentLongitude = document.getElementById("currentLongitude");
+function updateMyLocation(position) {
+  var currentLatitudeValue = document.getElementById("currentLatitudeValue");
+  var currentLongitudeValue = document.getElementById("currentLongitudeValue");
+  var latitudeValue = document.getElementById("latitudeValue");
+  var longitudeValue = document.getElementById("longitudeValue");
+  var latitudeOffset = Math.random() / 100;
+  var longitudeOffset = Math.random() / 100;
+  var latitude;
+  var longitude;
 
-    // currentLatitude.innerHTML = "Current Latitude: ";
-    // currentLongitude.innerHTML = "Current Longitude: ";
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+  if (currentLatitudeValue.innerHTML.length < 1) {
+    latitude = latitudeValue.innerHTML;
+    latitude = parseFloat(latitude);
+  } else {
+    latitude = currentLatitudeValue.innerHTML;
+    latitude = parseFloat(latitude);
+  }
 
-    latitude += Math.random() / 100;
-    longitude -= Math.random() / 100;
-    
-    // currentLatitude.innerHTML += String(latitude);
-    // currentLongitude.innerHTML += String(longitude);
-    console.log(`Current Latitude:${latitude}`);
-    currentLatitude.innerHTML = `Current Latitude:${latitude}`;
-} 
+  if (currentLongitudeValue.innerHTML.length < 1) {
+    longitude = longitudeValue.innerHTML;
+    longitude = parseFloat(longitude);
+  } else {
+    longitude = currentLongitudeValue.innerHTML;
+    longitude = parseFloat(longitude);
+  }
+
+  var updatedLatitude = latitude + latitudeOffset;
+  var updatedLongitude = longitude - longitudeOffset;
+
+  currentLatitudeValue.innerHTML = updatedLatitude;
+  currentLongitudeValue.innerHTML = updatedLongitude;
+}
 
 function getLocation() {
-
-    // asynchronous call with callback function specified
-    navigator.geolocation.getCurrentPosition(displayLocation);
-
+  // asynchronous call with callback function specified
+  navigator.geolocation.getCurrentPosition(displayLocation);
 }
 
 // callback function on success
 function displayLocation(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
 
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-
-    document.getElementById("latitude").innerHTML = 
-            "Latitude: " + latitude;
-    document.getElementById("longitude").innerHTML = 
-            "Longitude: " + longitude;
- 
-
+  document.getElementById("latitudeValue").innerHTML = latitude;
+  document.getElementById("longitudeValue").innerHTML = longitude;
 }
 // Handling error
 function handleError(error) {
-    switch(error.code) {
-        case 1:
-            updateStatus("The user denied permission");
-            break;
-        case 2:
-            updateStatus("Position is unavailable");
-            break;
-        case 3:
-            updateStatus("Timed out");
-            break;
-    }
+  switch (error.code) {
+    case 1:
+      updateStatus("The user denied permission");
+      break;
+    case 2:
+      updateStatus("Position is unavailable");
+      break;
+    case 3:
+      updateStatus("Timed out");
+      break;
+  }
 }
 function updateStatus(message) {
-    document.getElementById("status").innerHTML = 
-        "<strong>Error</strong>: " + message;
+  document.getElementById("status").innerHTML =
+    "<strong>Error</strong>: " + message;
 }
-
